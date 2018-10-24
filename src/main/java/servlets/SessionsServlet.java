041cd793707b57c4a3533b4e6818bd6f4e0aaf9d
tv.unimol <doslovno@gmail.com>
2018-10-24 13:04:14 +0300

    implement doGet

--- src/main/java/servlets/SessionsServlet.java
+++ src/main/java/servlets/SessionsServlet.java
@@ -1,6 +1,7 @@
 package servlets;
 
 import accounts.AccountService;
+import accounts.UserProfile;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -18,7 +19,15 @@ public class SessionsServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        String sessionId = req.getSession().getId();
+        UserProfile userProfile = accountService.getUserBySessionId(sessionId);
+        if (userProfile == null) {
+            resp.setContentType("text/html;charset=utf-8");
+            resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        } else {
+            resp.setContentType("text/html;charset=utf-8");
+            resp.setStatus(HttpServletResponse.SC_OK);
+        }
     }
 
     @Override
