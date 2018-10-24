eef285253aee9b4963453e19cae3c0e817260249
tv.unimol <doslovno@gmail.com>
2018-10-24 11:24:20 +0300

    add AccountService and handlers in main()

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,14 +1,29 @@
 package main;
 
+import accounts.AccountService;
+import accounts.UserProfile;
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 
 public class Main {
     public static void main(String[] args) throws Exception{
-        ServletContextHandler handler = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        AccountService accountService = new AccountService();
+        accountService.addNewUser(new UserProfile("admin"));
+        accountService.addNewUser(new UserProfile("test"));
+
+        ServletContextHandler contextHandler = new ServletContextHandler(ServletContextHandler.SESSIONS);
+
+        ResourceHandler resourceHandler = new ResourceHandler();
+        resourceHandler.setResourceBase("public_html");
+
+        HandlerList handlerList = new HandlerList();
+        handlerList.setHandlers(new Handler[]{resourceHandler, contextHandler});
 
         Server server = new Server(8080);
-        server.setHandler(handler);
+        server.setHandler(handlerList);
 
         server.start();
         System.out.println("Server started");
