d8b399b3928e0a5ece5a27e946096cc0cbd253f6
tv.unimol <doslovno@gmail.com>
2018-10-24 11:16:49 +0300

    delete MirrorRequestsServlet

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,14 +2,10 @@ package main;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
-import servlets.MirrorRequestsServlet;
 
 public class Main {
     public static void main(String[] args) throws Exception{
-        MirrorRequestsServlet mirrorRequestsServlet = new MirrorRequestsServlet();
         ServletContextHandler handler = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        handler.addServlet(new ServletHolder(mirrorRequestsServlet), "/mirror");
 
         Server server = new Server(8080);
         server.setHandler(handler);
