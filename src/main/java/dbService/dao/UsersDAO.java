6eea22eae9517dc2771e94f01f5c1a761d08fae4
manifest <doslovno@gmail.com>
2018-10-26 19:12:09 +0300

    add UsersDAO class

---
+++ src/main/java/dbService/dao/UsersDAO.java
@@ -0,0 +1,28 @@
+package dbService.dao;
+
+import dataSets.UsersDataSet;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+
+public class UsersDAO {
+
+    private Session session;
+
+    public UsersDAO(Session session) {
+        this.session = session;
+    }
+
+    public UsersDataSet get(long id) {
+        return session.get(UsersDataSet.class, id);
+    }
+
+    public long getUserId(String name) {
+        Criteria criteria = session.createCriteria(UsersDataSet.class);
+        return ((UsersDataSet) criteria.add(Restrictions.eq("name", name)).uniqueResult()).getId();
+    }
+
+    public long insertUser(String name) {
+        return (Long) session.save(new UsersDataSet(name));
+    }
+}
