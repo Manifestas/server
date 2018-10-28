bed53d4d31d9ac8f3b0d1fc19be29fe81886e761
tv.unimol <doslovno@gmail.com>
2018-10-28 13:53:30 +0300

    add ChatWebSocket class

---
+++ src/main/java/chat/ChatWebSocket.java
@@ -0,0 +1,28 @@
+package chat;
+
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+
+import java.io.IOException;
+
+@WebSocket
+public class ChatWebSocket {
+
+    private Session session;
+
+    @OnWebSocketConnect
+    public void onOpen(Session session) {
+        this.session = session;
+    }
+
+    @OnWebSocketMessage
+    public void onMessage(String data) {
+        try {
+            session.getRemote().sendString(data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
