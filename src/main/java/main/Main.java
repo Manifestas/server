eaa7b3460e1fc705f6aad9b6735b9e0f484acfe2
tv.unimol <doslovno@gmail.com>
2018-11-04 18:30:47 +0300

    add TestResourceServerController and ResourceServlet in main()

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -8,15 +8,24 @@ import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import resources.TestResource;
+import resources.TestResourceServer;
+import resources.TestResourceServerController;
+import resources.TestResourceServerControllerMBean;
+import servlets.ResourceServlet;
 import servlets.SessionsServlet;
 import servlets.SignInServlet;
 import servlets.SignUpServlet;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        TestResourceServer resourceServer = new TestResourceServer(new TestResource());
+        TestResourceServerControllerMBean resourceServerController = new TestResourceServerController(resourceServer);
+
         AccountService accountService = new AccountService();
 
         ServletContextHandler contextHandler = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        contextHandler.addServlet(new ServletHolder(new ResourceServlet(resourceServerController)), "/resources");
         contextHandler.addServlet(new ServletHolder(new SessionsServlet(accountService)), "/api/v1/sessions");
         contextHandler.addServlet(new ServletHolder(new SignUpServlet(accountService)), "/signup");
         contextHandler.addServlet(new ServletHolder(new SignInServlet(accountService)), "/signin");
